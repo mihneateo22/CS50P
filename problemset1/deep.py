fa522290140a155d8aca6b37065d178970a3a492
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2026-09-24 15:38:25 +0300

    coded using the match expression

---
+++ problemset1/deep.py
@@ -0,0 +1,6 @@
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").lower()
+match answer:
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
