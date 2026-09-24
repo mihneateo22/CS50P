32d15c99bb50517bfab79ecdaea7ec9da8bb4ac1
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2026-09-24 19:08:04 +0300

    added problem set 2

---
+++ problemset2/camelcase.py
@@ -0,0 +1,8 @@
+camelCase = input("camelCase: ")
+print("snake_case: ", end="")
+for character in camelCase:
+    if character.isupper():
+        character = character.lower()
+        print('_', end="")
+    print(character, end="")
+print()
