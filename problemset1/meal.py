05df63a68ec6a3b4ef44a933cc024f156c212b84
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2026-09-24 16:20:24 +0300

    coded using the .split() method

---
+++ problemset1/meal.py
@@ -0,0 +1,19 @@
+def main():
+    time = input("What time is it? ")
+    time = convert(time)
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
+
+
+def convert(time):
+    hour, minute = time.split(":")
+    new_time = round(float(hour) + float(minute) / 60, 2)
+    return new_time
+
+
+if __name__ == "__main__":
+    main()
