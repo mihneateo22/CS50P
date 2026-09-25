44abfe9b254b6ab4f252c3ba7ecc36e0ddeca7d9
Mihnea Teodorescu <teodorescu.mihnea@gmail.com>
2026-09-25 13:21:35 +0300

    coded using the dict with try and except

---
+++ problemset3/grocery.py
@@ -0,0 +1,17 @@
+groceries = {}
+
+while True:
+    try:
+        item = input().upper()
+    except KeyError:
+        pass
+    except EOFError:
+        print()
+        break
+    if item in groceries:
+        groceries[item] += 1
+    else: 
+        groceries[item] = 1
+
+for item in sorted(groceries):
+    print(groceries[item], item)
